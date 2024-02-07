337867fb1a837bec70c3c9647d05e3b260e0fc7b
Tommy Phelps <tommydustinphelps@gmail.com>
2024-02-06 23:30:01 -0600

    Create and populate PHP file for sending emails

---
+++ contactForm.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_post["submit"])) {
+    $name = $_POST["name"];
+    $mailFrom = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    $mailTo = "tommyphelps16@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an email from ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
