77ab668e7e2ea7f70feeffe6c93720d047497d22
Tommy Phelps <tommydustinphelps@gmail.com>
2024-02-24 15:34:16 -0600

    Change footer and contact section

--- index.php
+++ index.php
@@ -120,8 +120,12 @@
         </main>
 
         <footer>
-            <p>VRHomeImprovements LLC</p>
-            <p>Where we help everyone with their needs.</p>
+            <div class="social-links">
+                <a href="#"><img src="images/facebook.png" alt="Facebook"></a>
+                <a href="#"><img src="images/twitter.png" alt="Twitter"></a>
+                <a href="#"><img src="images/instagram.png" alt="Instagram"></a>
+            </div>
+            <p>&copy; 2024 VR Home Improvements LLC. All rights reserved.</p>
         </footer>
     </body>
 </html>
