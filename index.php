e01cc56074dee551120044a857426dbe3ee5532c
Tommy Phelps <tommydustinphelps@gmail.com>
2024-02-24 13:54:57 -0600

    Various changes

--- index.php
+++ index.php
@@ -24,86 +24,102 @@
         <div class="hero">
             <h1>Welcome to VR Home Improvements LLC - Elevating Homes and Spaces in Oklahoma</h1>
 
-            <p>Embark on a journey of unparalleled craftsmanship with VR Home Improvements LLC, your trusted partner for comprehensive construction services in Oklahoma. From groundbreaking ideas to structurally sound implementations, we bring dreams to life through meticulous construction and home improvement services.</p>
+            <p>Embark on a journey of unparalleled craftsmanship with VR Home Improvements LLC, your trusted 
+            partner for comprehensive construction services in Oklahoma. From groundbreaking ideas to structurally
+            sound implementations, we bring dreams to life through meticulous construction and home improvement services.</p>
         </div>
 
         <div class="about">
             <div>
-            <h2>About Us</h2>
-            <p>At VR Home Improvements LLC, we take pride in being the cornerstone of excellence in the construction industry in Oklahoma. With a commitment to quality, innovation, and client satisfaction, we have established ourselves as a reliable choice for all your home improvement needs.</p>
+                <h2>About Us</h2>
+                <p>At VR Home Improvements LLC, we take pride in being the cornerstone of excellence in the construction 
+                industry in Oklahoma. With a commitment to quality, innovation, and client satisfaction, we have established 
+                ourselves as a reliable choice for all your home improvement needs.</p>
 
-            <p>Our team of skilled professionals possesses a wealth of experience in transforming spaces into havens of comfort and style. From concept to completion, we approach each project with unwavering dedication, ensuring that your vision is not just realized but exceeded.</p>
+                <p>Our team of skilled professionals possesses a wealth of experience in transforming spaces into havens of 
+                comfort and style. From concept to completion, we approach each project with unwavering dedication, 
+                ensuring that your vision is not just realized but exceeded.</p>
 
-            <p>We believe in transparency, clear communication, and a client-centric approach. Our mission is to not just build structures but to create homes that stand the test of time, reflecting the unique personality and aspirations of our clients.</p>
+                <p>We believe in transparency, clear communication, and a client-centric approach. Our mission is to not 
+                just build structures but to create homes that stand the test of time, reflecting the unique 
+                personality and aspirations of our clients.</p>
             </div>
 
             <div>
-            <h3>Why Choose Us?</h3>
-            <ul>
-                <div>
-                <li>Expertise and Experience:</li>
-                <p>With years of experience in providing construction services in [Your City], VR Home Improvements LLC brings a wealth of knowledge and skills to every project.</p>
-                </div>
-                <div>
-                <li>Quality Assurance:</li>
-                <p>We adhere to the highest standards of craftsmanship, using premium materials to ensure the longevity and durability of our work, delivering top-notch home improvement solutions.</p>
-                </div>
-                <div>
-                <li>Timely Delivery:</li>
-                <p>Efficient project management ensures that your project is completed on schedule without compromising quality, offering prompt and reliable construction services.</p>
-                </div>
-                <div>
-                <li>Client-Centric Approach:</li>
-                <p>Your vision is our guide. We work closely with our clients, fostering open communication to ensure that every detail is considered, and expectations are met for a personalized home improvement experience.</p>
-                </div>
-            </ul>
+                <h3>Why Choose Us?</h3>
+                <ul>
+                    <div>
+                        <li>Expertise and Experience:</li>
+
+                        <p>With years of experience in providing construction services in Oklahoma, VR Home Improvements LLC 
+                        brings a wealth of knowledge and skills to every project.</p>
+                    </div>
+                    <div>
+                        <li>Quality Assurance:</li>
+
+                        <p>We adhere to the highest standards of craftsmanship, using premium materials to ensure the 
+                        longevity and durability of our work, delivering top-notch home improvement solutions.</p>
+                    </div>
+                    <div>
+                        <li>Timely Delivery:</li>
+
+                        <p>Efficient project management ensures that your project is completed on schedule without 
+                        compromising quality, offering prompt and reliable construction services.</p>
+                    </div>
+                    <div>
+                        <li>Client-Centric Approach:</li>
+
+                        <p>Your vision is our guide. We work closely with our clients, fostering open communication to ensure 
+                        that every detail is considered, and expectations are met for a personalized home improvement experience.</p>
+                    </div>
+                </ul>
             </div>
         </div>
 
         <div class="services">
             <h2>Services</h2>
             <ul>
-            <div>
-                <li>Framing</li>
-                <p>Our framing services form the structural backbone of your project. Precision and attention to detail are at the forefront of our framing expertise, ensuring a solid foundation for the rest of the construction process.</p>
-            </div>
+                <div>
+                    <li>Framing</li>
+                    <p>Our framing services form the structural backbone of your project. Precision and attention to detail are at the forefront of our framing expertise, ensuring a solid foundation for the rest of the construction process.</p>
+                </div>
 
-            <div>
-                <li>Plumbing</li>
-                <p>Experience seamless functionality with our expert plumbing services. From installations to repairs, our skilled plumbers deliver solutions that stand up to the test of time, enhancing the efficiency and reliability of your plumbing systems.</p>
-            </div>
+                <div>
+                    <li>Plumbing</li>
+                    <p>Experience seamless functionality with our expert plumbing services. From installations to repairs, our skilled plumbers deliver solutions that stand up to the test of time, enhancing the efficiency and reliability of your plumbing systems.</p>
+                </div>
 
-            <div>
-                <li>Roofing</li>
-                <p>Protect your investment with our comprehensive roofing solutions. Whether it's repairs, installations, or replacements, our roofing specialists combine skill and quality materials to provide a shelter that stands strong against the elements.</p>
-            </div>
+                <div>
+                    <li>Roofing</li>
+                    <p>Protect your investment with our comprehensive roofing solutions. Whether it's repairs, installations, or replacements, our roofing specialists combine skill and quality materials to provide a shelter that stands strong against the elements.</p>
+                </div>
 
-            <div>
-                <li>Electrical</li>
-                <p>Illuminate your space with our electrical services. From wiring to installations, our electricians bring safety and innovation to every project, ensuring that your electrical systems meet modern standards.</p>
-            </div>
+                <div>
+                    <li>Electrical</li>
+                    <p>Illuminate your space with our electrical services. From wiring to installations, our electricians bring safety and innovation to every project, ensuring that your electrical systems meet modern standards.</p>
+                </div>
             </ul>
         </div>
 
         <main>
             <div class="contact-container">
-            <div>
-                <h2>Get in touch</h2>
-                <h3>Give us a call</h3>
-                <p>(918 649-0217</p>
-                <h3>Our address</h3>
-                <p>Porter, OK</p>
-            </div>
+                <div>
+                    <h2>Get in touch</h2>
+                    <h3>Give us a call</h3>
+                    <p>(918 649-0217</p>
+                    <h3>Our address</h3>
+                    <p>Porter, OK</p>
+                </div>
 
-            <div>
-                <form class="contact-form" action"contactForm.php" method="post">
-                <input type="text" name="name" placeholder="Your name" required>
-                <input type="text" name="email" placeholder="Your email" required>
-                <input type="text" name="subject" placeholder="Subject">
-                <input name="message" placeholder="Your message" required>
-                <button type="submit" name="Submit">Send</button>
-                </form>
-            </div>
+                <div>
+                    <form class="contact-form" action"contactForm.php" method="post">
+                    <input type="text" name="name" placeholder="Your name" required>
+                    <input type="text" name="email" placeholder="Your email" required>
+                    <input type="text" name="subject" placeholder="Subject">
+                    <input name="message" placeholder="Your message" required>
+                    <button type="submit" name="Submit">Send</button>
+                    </form>
+                </div>
             </div>
         </main>
 
