65db893c03dd8b6d107f346b9dc2d4f62f5de75c
Tommy Phelps <tommydustinphelps@gmail.com>
2024-03-02 22:55:18 -0600

    Switch to bootstrap carousel, remove old carousel styles

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
     <head>
     <title>VR Home Improvements LLC</title>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
 
     <body>
@@ -100,32 +102,49 @@
             </ul>
         </div>
 
-        <div class="img-carousel">
-            <div class="slide">
-                <div class="img-pair">
-                    <img src="images/project1_before.jpg" alt="Project 1 Before">
-                    <img src="images/project1_after.jpg" alt="Project 1 After">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="images/project1_before.jpg" alt="First slide">
+                    <img class="d-block w-100" src="images/project1_after.jpg" alt="First slide">
+
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Project 1</h5>
+                        <p>Project 1 before and after.</p>
+                    </div>
                 </div>
-                <div class="caption">Project 1 Before and After</div>
-            </div>
-            <div class="slide">
-                <div class="img-pair">
-                    <img src="images/project2_before.jpg" alt="Project 2 Before">
-                    <img src="images/project2_after.jpg" alt="Project 2 After">
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="images/project2_before.jpg" alt="First slide">
+                    <img class="d-block w-100" src="images/project2_after.jpg" alt="First slide">
+
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Project 2</h5>
+                        <p>Project 2 before and after.</p>
+                    </div>
                 </div>
-                <div class="caption">Project 2 Before and After</div>
-            </div>
-            <div class="slide">
-                <div class="img-pair">
-                    <img src="images/project3_before.jpg" alt="Project 3 Before">
-                    <img src="images/project3_after.jpg" alt="Project 3 After">
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="images/project3_before.jpg" alt="First slide">
+                    <img class="d-block w-100" src="images/project3_after.jpg" alt="First slide">
+
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Project 3</h5>
+                        <p>Project 3 before and after.</p>
+                    </div>
                 </div>
-                <div class="caption">Project 3 Before and After</div>
             </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
-        <button id="prevBtn" onclick="prevSlide()">Previous</button>
-        <button id="nextBtn" onclick="nextSlide()">Next</button>
-        <script src="script.js"></script>
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
         <main class="contact-container" id="contact">
             <div>
