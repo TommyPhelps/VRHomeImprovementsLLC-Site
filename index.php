fe65beba601fef77a2b70cff05accee5462ef16e
Tommy Phelps <tommydustinphelps@gmail.com>
2024-03-04 21:18:42 -0600

    Add review section for website

--- index.php
+++ index.php
@@ -141,6 +141,18 @@
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         </div>
 
+        <div class="reviews-container" id="reviews">
+            <h2>Reviews</h2>
+            <div class="review-container">
+                <div>
+                    <img src="images/bJohnson.jpg" alt="Image of a woman">
+                    <p><strong>Bianca Johnson</strong></p>
+                    <p>December 4, 2019</p>
+                </div>
+                <p>Such a hard working group and they do such a great job.</p>
+            <div>
+        </div>
+
         <main class="contact-container" id="contact">
             <div>
                 <h2>Contact Us</h2>
