4df6d6c84c11c5d4af3b3e225697288bf8cc22fa
Tommy Phelps <tommydustinphelps@gmail.com>
2024-03-06 22:06:43 -0600

    Remove img-carousel from project
    Removed the image carousel and bootstrap code. Will add new implementation of project images soon

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-    <title>VR Home Improvements LLC</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <title>VR Home Improvements LLC</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="styles.css">
     </head>
 
     <body>
@@ -102,44 +100,6 @@
                     <img src="images/electrical.jpg" alt="electrical">
                 </div>
             </ul>
-        </div>
-
-        <div class="img-carousel" id="projects">
-            <h2>Projects</h2>
-            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <div class="d-flex justify-content-center">
-                            <img class="w-50" src="images/project1_before.jpg" alt="First Project Before">
-                            <img class="w-50" src="images/project1_after.jpg" alt="First Project After">
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <div class="d-flex justify-content-center">
-                            <img class="w-50" src="images/project2_before.jpg" alt="Second Project Before">
-                            <img class="w-50" src="images/project2_after.jpg" alt="Second Project After">
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <div class="d-flex justify-content-center">
-                            <img class="w-50" src="images/project3_before.jpg" alt="Third Project Before">
-                            <img class="w-50" src="images/project3_after.jpg" alt="Third Project After">
-                        </div>
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
-
-            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         </div>
 
         <div class="reviews-container" id="reviews">
