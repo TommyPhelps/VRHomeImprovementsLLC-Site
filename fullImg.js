aec8829a6106fcc8bde629df7ec5c8ec047ab4a5
Tommy Phelps <tommydustinphelps@gmail.com>
2024-04-01 07:21:50 -0500

    Optimize image loading in fullImg.js

--- fullImg.js
+++ fullImg.js
@@ -5,9 +5,17 @@ var projects = document.querySelectorAll(".project");
 projects.forEach(function(project) {
     project.addEventListener("click", function() {
         modal.style.display = "block";
-        modalImg.src = this.querySelector("img").src;
+        modalImg.src = "";
+        const lowResSrc = this.querySelector("img").src;
+        modalImg.src = lowResSrc;
         modalImg.style.maxWidth = "100%";
         modalImg.style.height = "auto";
+
+        const highResImg = new Image();
+        highResImg.src = lowResSrc.replace(/(\.)(png|jpe?g|webp)/, "@2x$1$2");
+        highResImg.onload = function() {
+            modalImg.src = highResImg.src;
+        };
     });
 });
 
